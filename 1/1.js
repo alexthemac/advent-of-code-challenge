42f9b60e5eaf9fd665df38a64cacf2b4155c00b7
Alex MacLure <almaclure@gmail.com>
2021-12-17 17:14:15 -0500

    Finish part 2 of day 1

--- 1/1.js
+++ 1/1.js
@@ -1,5 +1,7 @@
+/////=--------------------PART1--------------------=//////
 // The first order of business is to figure out how quickly the depth increases, just so you know what you're dealing with - you never know if the keys will get carried into deeper water by an ocean current or a fish or something.
 // To do this, count the number of times a depth measurement increases from the previous measurement. (There is no measurement before the first measurement.)
+
 
 //Create array of initial data
 let fs = require('fs');
@@ -16,6 +18,22 @@ for (let i = 1; i < dataArray.length; i++) {
     countDepthIncrease++;
   }
   prevNumber = dataArray[i];
-}
+};
 
 console.log("increase", countDepthIncrease);
+
+/////=--------------------PART2--------------------=//////
+
+let prevSum = dataArray[0] + dataArray[1] + dataArray[2];
+let countSumIncrease = 0;
+
+//Find number of times depth increases
+for (let i = 1; i < dataArray.length; i++) {
+  let currentSum = dataArray[i] + dataArray[i + 1] + dataArray[i + 2];
+  if (currentSum > prevSum) {
+    countSumIncrease++;
+  }
+  prevSum = currentSum;
+};
+
+console.log("increase", countSumIncrease);
---
+++ 2/2.js
@@ -0,0 +1,15 @@
+/////=--------------------PART1--------------------=//////
+
+//Create array of initial data
+let fs = require('fs');
+let content = fs.readFileSync('input.txt', 'utf8')
+let dataArray = content.split("\n");
+
+console.log(dataArray);
+
+let horizontalPosition = 0;
+let verticalPosition = 0;
+
+dataArray.forEach(() => {
+  
+})
