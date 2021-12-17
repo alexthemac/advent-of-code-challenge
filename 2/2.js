818156b5b02fc4b4a8b659aafdbfe04c8d152dbd
Alex MacLure <almaclure@gmail.com>
2021-12-17 17:27:16 -0500

    Complete part 1 of day 2

--- 2/2.js
+++ 2/2.js
@@ -1,15 +1,32 @@
-/////=--------------------PART1--------------------=//////
+/////=--------------------PART1--------------------=/////
 
 //Create array of initial data
 let fs = require('fs');
 let content = fs.readFileSync('input.txt', 'utf8')
 let dataArray = content.split("\n");
-
-console.log(dataArray);
-
 let horizontalPosition = 0;
 let verticalPosition = 0;
 
-dataArray.forEach(() => {
-  
-})
+dataArray.forEach((data) => {
+  const movementArray = data.split(" ");
+  movementArray[1] = parseInt(movementArray[1]);
+  console.log(movementArray)
+  switch (movementArray[0]) {
+    case "forward":
+      horizontalPosition += movementArray[1];
+      break;
+    case "backward":
+      horizontalPosition -= movementArray[1];
+      break;
+    case "down":
+      verticalPosition += movementArray[1];
+      break;
+    case "up":
+      verticalPosition -= movementArray[1];
+      break;
+  };
+});
+
+console.log("horizontal: ", horizontalPosition, "vertical: ", verticalPosition, "horizontal * vertical: ", horizontalPosition * verticalPosition);
+
+/////=--------------------PART2--------------------=/////
